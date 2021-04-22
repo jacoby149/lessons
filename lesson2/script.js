614cfc76699749d20f10a0ad085f70935f1e5f14
Jacob Hoffman <jackstudent149@gmail.com>
2021-04-22 11:34:12 -0400

    clean comments in the code

--- lesson2/script.js
+++ lesson2/script.js
@@ -1,4 +1,7 @@
-//ignore this. this 
+////////////////////////////////
+///// ignore these 
+////////////////////////////////
+
 // external js: isotope.pkgd.js
 $('.grid').isotope({
     itemSelector: '.grid-item',
@@ -12,30 +15,38 @@ Array.prototype.random = function () {
 }
 
 
+///////////////////////////
+//box modifying functions
+///////////////////////////
+
 //sets the color of a box in row i, column j to color c
 function setColor(c, i, j) {
     var row = document.getElementsByClassName("grid")[i];
     var box = row.getElementsByClassName("grid-item")[j];
     box.style.backgroundColor = c;
 }
-
 //adds a grid row to the bigBox div element.
 function addRow() {
     bigBox.innerHTML += `<div class="grid"></div>`;
 }
 //adds a box to the grid row with specified number.
 function addBox(rowNumber) {
-    var rows = [...bigBox.getElementsByClassName('grid')];
-    var row = rows[rowNumber];
+    var row = bigBox.getElementsByClassName('grid')[rowNumber];
     row.innerHTML += `<div class="grid-item"></div>`
 }
 
 
+///////////////////////////////
+////// grid functions
+///////////////////////////////
+
 //gets alot of grid items all in the same row
 function grid1(n) {
+    bigBox.style.width = 'auto';
+    bigBox.style.maxWidth = 900;
+    var interval = 100;
     var colors = ["red", "orange", "yellow"];
     addRow();
-    var interval = 200
     for (let i = 0; i < n; i++) {
         //addBox(0);
         setTimeout(() => { addBox(0); setColor(colors.random(), 0, i); }, i * interval);
@@ -43,7 +54,7 @@ function grid1(n) {
 }
 //get a rowsxcolumns sized grid with rows rows.
 function grid2(numRows, numCols) {
-    var boxNumber = 0;
+    bigBox.style.width = 900;
     var interval = 200;
     //don't use var in for loops.
     //change the lets to var, and watch it break.
@@ -52,21 +63,20 @@ function grid2(numRows, numCols) {
         for (let j = 0; j < numCols; j++) {
             //addBox(i);
             setTimeout(() => { addBox(i); setColor("white", i, j) }, (-i + j) * interval)
-            boxNumber++;
         }
     }
 }
 
+//write your own function grid3 from scratch
+
+
+////////////////////////////////
+//// choosing a grid
+////////////////////////////////
 
 var gridChoice = 1
 if (gridChoice == 1) {
-    bigBox.style.width = 'auto';
-    bigBox.style.maxWidth = 900;
     grid1(100);
 } else if (gridChoice == 2) {
-    bigBox.style.width = 900;
     grid2(5, 20);
-} else if (gridChoice == 3) {
-    bigBox.style.width = 900;
-    grid3(5, 20);
-} //add your own grid4 function here.
+}  //add your own grid3 function here.
