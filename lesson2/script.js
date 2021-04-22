94e96474dd373724fa7e6c928880408c790e5103
Jacob Hoffman <jackstudent149@gmail.com>
2021-04-22 09:58:36 -0400

    grid 2 done

--- lesson2/script.js
+++ lesson2/script.js
@@ -13,20 +13,40 @@ function addRow() {
 }
 
 //adds a box to the grid row with specified number.
-function addBox(rowNumber) {
-    var rows = bigBox.getElementsByClassName('grid');
+function addBox(rowNumber, color = "#0d8") {
+    var rows = [...bigBox.getElementsByClassName('grid')];
     var row = rows[rowNumber];
-    row.innerHTML += `<div class="grid-item"></div>`
+    row.innerHTML += `<div class="grid-item" style="background-color:${color};"></div>`
 }
 
 //gets alot of grid items all in the same row
 function grid1(n) {
     addRow();
-    for (var i = 0; i < n; i++) {
-        addBox(0);
-        //setTimeout(() => addBox(0), i * 200);
+    for (let i = 0; i < n; i++) {
+        //addBox(0);
+        setTimeout(() => addBox(0), i * 200);
     }
 }
 
+//get a rowsxcolumns sized grid with rows rows.
+function grid2(numRows, numCols) {
+    var boxNumber = 0
+    //don't use var in for loops.
+    //change the lets to var, and watch it break.
+    for (let i = 0; i < numRows; i++) {
+        addRow();
+        for (let j = 0; j < numCols; j++) {
+            //addBox(i);
+            setTimeout(() => addBox(i), (-i + j) * 200)
+            boxNumber++;
+        }
+    }
+}
+
+
 bigBox.style.width = 'auto';
+bigBox.style.maxWidth = 900;
 grid1(161);
+
+//bigBox.style.width = 900;
+//grid2(5, 20);
