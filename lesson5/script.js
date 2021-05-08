0d0727ce1e5bbf0b50453c1446b86e52434ffa36
Jacob Hoffman <jackstudent149@gmail.com>
2021-05-08 18:16:06 -0400

    lesson 5 done

--- lesson5/script.js
+++ lesson5/script.js
@@ -35,6 +35,7 @@ function addBox(i) {
 function clearAll() {
     gridContainer.innerHTML = "";
 }
+//sets the color of square with coordinate (i,j) to c
 function setColor(c, i, j) {
     var box = getBox(i, j)
     box.style.backgroundColor = c;
@@ -45,14 +46,19 @@ function setColor(c, i, j) {
 ///// initiallize snake game
 /////////////////////////////////
 var width = 15; var height = 15;
-var vX = 0; var vY = 1; //speed of the snake
-var snake =
-    [
-        { x: 2, y: 1 },
-        { x: 2, y: 2 },
-        { x: 2, y: 3 },
-        { x: 3, y: 3 }
+var vX = 1; var vY = 0; //speed of the snake
+var snake = null;
+
+function initSnake() {
+    snake = [
+        { x: 2, y: 1 }, //x y coordinate of the head snake segment
+        { x: 2, y: 2 }, //snake segment
+        { x: 2, y: 3 }, //snake segment
+        { x: 3, y: 3 }  //x y of the tail snake segment
     ];
+    //initializes var snake to an array of snake segments
+    //snake segment - a box that is a part of the snake.
+}
 
 
 //////////////////////////////////
@@ -61,11 +67,18 @@ var snake =
 function move() {
     var head = snake[0]; //get the current head of the snake
     var newHead = {
-        x: (width + head.x + vX) % width,
-        y: (height + head.y + vY) % height
+        x: head.x + vX,
+        y: head.y + vY
     };
     snake.unshift(newHead); //extend the head of the snake
-    snake.pop(); //recede the tail of the snake
+    //the unshift function is called on the snake array, 
+    //it adds the newhead input argument at the beginning of the array.
+
+    //TODO recede the tail of the snake with the pop function above!
+    //you can call the pop function on any array
+    //the pop function takes no input arguments
+    //the pop function removes the last element of an array
+    //hint : read the snake.unshift comments
 }
 
 function model() {
@@ -78,10 +91,11 @@ function model() {
 //////////////////////////////////
 function control(event) {
     switch (event.code) {
+        case "KeyR": initSnake(); break;
         case "KeyS": vX = 0; vY = 1; break;
-        case "KeyW": vX = 0; vY = -1; break;
-        case "KeyA": vX = -1; vY = 0; break;
-        case "KeyD": vX = 1; vY = 0; break;
+        case "KeyW": break;
+        case "KeyA": break;
+        case "KeyD": break;
         default: return; // Quit when this doesn't handle the key event.
     }
 }
@@ -101,7 +115,12 @@ function viewMap(width, height) {
 }
 function viewSnake() {
     for ([i, segment] of snake.entries()) {
-        setColor("red", segment.y, segment.x); // draw your location
+        //TODO make the snake visible using the setColor function
+        //setColor takes a color and the coordinates of a box to color
+        //use the coordinates of the segment, and a color of your choice as input
+        //hint : see the use of setColor in viewMap
+        console.log(segment.y);
+        console.log(segment.x);
     }
 }
 function view() {
@@ -119,6 +138,7 @@ function frame() {
     view();
 }
 function startGame() {
+    initSnake();
     setInterval(frame, 100);
     document.addEventListener('keydown', control); //listen for wasd controls
 }
